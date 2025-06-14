3c5530f6829075d275b63936852e3e1417ef844b
Moncayo1329 <moncayomichael23@gmail.com>
2025-06-14 17:09:33 +0100

    scraping novak news

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-const Table = require('cli-table');
+const puppeteer = require('puppeteer');
 
-const options = {
-url: 'https://news.google.com/search?q=novak%20djokovic&hl=es-419&gl=US&ceid=US%3Aes-419',
-transform: body => cheerio.load(body)
-};
 
-rp(options)
-  .then($ => {
-    const tabla = new Table({
-      head: ['Año', 'Título', 'Enlace'],
-      colWidths: [60, 60]
-    });
+async function openWebPage() {
 
-    $('a').each((i, el) => {
-      const titulo = $(el).text().trim();
-      const link = $(el).attr('href');
-
-      if (titulo.toLowerCase().includes('novak')) {
-        const url = link.startsWith('http') ? link : `https://news.google.com${link.replace('./', '/')}`;
-        tabla.push([titulo, url]);
-      }
-    });
-
-    if (tabla.length === 0) {
-      console.log('❌ No se encontraron noticias sobre Novak.');
-    } else {
-      console.log(tabla.toString());
+  const browser = await puppeteer.launch({
+      headless: false,
+      slowMo:400,
     }
-  })
-  .catch(err => {
-    console.error('❌ Error en el scraping:', err.message);
-  });
+  )
+ 
+  const page = await browser.newPage()
+
+  await  page.goto('https://www.puntodebreak.com/tag/novak-djokovic')
+
+await browser.close()
+
+}
+
+openWebPage()
+
