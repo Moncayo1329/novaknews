3d6b9808a2514a3c1c14a9c07fdf11d26f9ae557
Moncayo1329 <moncayomichael23@gmail.com>
2025-06-16 18:01:19 +0100

    still process

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ const puppeteer = require('puppeteer');
 async function scrapeNovakDjokovic() {
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 400,
+    slowMo: 200,
   });
 
   const page = await browser.newPage();
-  await page.goto('https://www.puntodebreak.com')
-  await page.click('a[href="/tag/novak-djokovic"]')
-  await new Promise (r => setTimeout(r,10000));
-  
-  const result = await page.evaluate(()=> {
-    document.querySelector('h1').innerText
+  await page.goto('https://quotes.toscrape.com')
+  const result = await page.evaluate(() => {
+const quotes = document.querySelectorAll('.quote')
+return quotes
+
   })
 
   console.log(result)
+
   await browser.close()
-
 }
-
+ 
 scrapeNovakDjokovic();
